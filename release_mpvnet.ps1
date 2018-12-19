ea8de8bd5a2711df6fbecf490e99aebd62586213
Frank Skare <frank.skare.de@gmail.com>
2018-12-19 23:42:32 +0100

    0.2.3

--- release_mpvnet.ps1
+++ release_mpvnet.ps1
@@ -1,7 +1,7 @@
 using namespace System.Diagnostics
 $exePath = "C:\Users\frank\Desktop\Projekte\mpvnet\mpvnet\bin\Debug\mpvnet.exe"
 $version = [FileVersionInfo]::GetVersionInfo($exePath).FileVersion
-$targetDir = "C:\Users\Frank\Desktop\mpv-net-" + $version
+$targetDir = "C:\Users\Frank\Desktop\mpv.net-" + $version
 Copy-Item C:\Users\frank\Desktop\Projekte\mpvnet\mpvnet\bin\Debug $targetDir -recurse
 $addonDir = $targetDir + "\Addons"
 remove-item $addonDir -Recurse -Include *vbnet.pdb, *mpvnet.exe, *mpvnet.exe.config, *mpvnet.pdb, *vbnet.dll
