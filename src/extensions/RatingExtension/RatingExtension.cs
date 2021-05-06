3161ba102219ecd9d1c8bab7757ebe7de8cd98b3
Frank Skare <frank.skare.de@gmail.com>
2021-05-06 16:42:07 +0200

    rating extension update

--- src/extensions/RatingExtension/RatingExtension.cs
+++ src/extensions/RatingExtension/RatingExtension.cs
@@ -1,17 +1,27 @@
 
 // This extension writes a rating to the filename of rated videos when mpv.net shuts down.
 
-// The input.conf defaults contain key bindings for this extension to set ratings.
+// The input.conf setup:
+
+// KP0 script-message rate-file 0     #menu: Extensions > Rating > 0stars
+// KP1 script-message rate-file 1     #menu: Extensions > Rating > 1stars
+// KP2 script-message rate-file 2     #menu: Extensions > Rating > 2stars
+// KP3 script-message rate-file 3     #menu: Extensions > Rating > 3stars
+// KP4 script-message rate-file 4     #menu: Extensions > Rating > 4stars
+// KP5 script-message rate-file 5     #menu: Extensions > Rating > 5stars
+// _   ignore                         #menu: Extensions > Rating > -
+// _   script-message rate-file about #menu: Extensions > Rating > About
 
 using System;
-using System.ComponentModel.Composition;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.IO;
+using System.Threading;
+
 using Microsoft.VisualBasic.FileIO;
 
 using mpvnet;
 using static mpvnet.Core;
-using System.Threading;
 
 namespace RatingExtension // the assembly name must end with 'Extension'
 {
